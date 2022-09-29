03389c0023503885766ea4ca64f691451219d855
CryptoRei <cryptoreismith@gmail.com>
2022-09-29 16:08:40 +0300

    Add line colors (but bug in buttons)

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 from random import randint
 
 # TODO: Заполнить списком цветов
-colors = ["r"] * 100
+colors = ["aqua", "orange", "hotpink", "lightslategray", "yellow", "springgreen", "blueviolet", "orangered", "royalblue", "green", "plum", "paleturquoise", "palegreen", "navy", "turquoise", "mediumvioletred", "darkgoldenrod", "fuchsia", "steelblue", "lightcoral", "thistle", "khaki", "chartreuse", "teal", "saddlebrown", "violet", "lemonchiffon", "blue", "olive", "red"]
 @dataclass
 class KZPowerREG:
     UC: float = 0
@@ -48,29 +48,39 @@ class MainWindow(QDialog):
         self.setWindowTitle("dataClass_graphics")
 
 
+        # Данные для графика
+        # Массив с массивом значений (длина y = num_of_lines)
+        #                             длина каждого y[i] = число значений)
+        self.y = []         # - двумерный
+        self.x = []         # - одномерный
+        # Отображаемые значения
+        self.shown_x = []    # - двумерный
+        self.shown_y = []    # - двумерный
+
         # Структура, данные которой отображаются на графике
-        self.data_line = []
+        self.data_lines = []
         self.pen = []
 
         # Массив чек боксов
-        self.choice_button = []
+        self.choice_buttons = []
         self.dictionary = data_class_dict
 
         # Заполняет массив кнопок
         for key in self.dictionary.keys():
             if type(self.dictionary.get(key)) == type(0):
-                self.choice_button.append(QCheckBox(key))
+                self.choice_buttons.append(QCheckBox(key))
             else:
                 key_values = self.dictionary.get(key).__dict__
                 for key_item in key_values:
-                    self.choice_button.append(QCheckBox(f"{key}: {key_item}"))
+                    self.choice_buttons.append(QCheckBox(f"{key}: {key_item}"))
 
         num_of_lines = 0
         layout_h = QHBoxLayout()
         layout_v = QVBoxLayout()
         # Добавляем кнопки на экран
         # и хорошо считаем количество выводимых параметров (aka линий aka кнопок)
-        for button in self.choice_button:
+        for button in self.choice_buttons:
+            button.setChecked(True)
             layout_v.addWidget(button)
             num_of_lines += 1
 
@@ -78,11 +88,54 @@ class MainWindow(QDialog):
         for i in range(num_of_lines):
             line_color = colors[i]
             self.pen.append(pg.mkPen(color = line_color))
-            self.data_line.append(self.graphWidget.plot([i], [i+2], pen = self.pen[i], symbol='+', symbolSize=10, symbolBrush=line_color))
+            self.data_lines.append(self.graphWidget.plot([i], [i+2], pen = self.pen[i], symbol='+', symbolSize=10, symbolBrush=line_color))
 
         layout_h.addWidget(self.graphWidget)
         layout_h.addLayout(layout_v)
         self.setLayout(layout_h)
+
+        # TODO: записать данные из data-Class
+        self.y = [] # - одномерный
+        self.x = [i for i in range(num_of_lines)]  # - одномерный
+        for i in range(num_of_lines):
+            self.y.append(self.x)
+            self.shown_x.append(self.x)
+
+
+        self.shown_y = self.y
+
+        #for i in range(len(self.y)):
+        #    print(f"self.y[i] = {self.y[i]}, self.shown_y[i] = {self.shown_y[i]}")
+
+
+        # Set update settings
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
+    def update_plot_data(self):
+        self.x = self.x[1:]
+        self.x.append(self.x[-1] + 1)
+        for i in range(len(self.y)):
+            self.y[i] = self.y[i][1:]
+            self.y[i].append(randint(0, 20))  # TODO: добавлять значения из data-Class
+            #print(f"1 - len(shown_y[{i}]) = {len(self.shown_y[i])}, len(y[{i}]) = {len(self.y[i])}")
+        for i in range(len(self.choice_buttons)):
+            button = self.choice_buttons[i]
+            if button.isChecked():
+                #print(f"2 - len(shown_y[{i}]) = {len(self.shown_y[i])}, len(y[{i}]) = {len(self.y[i])}")
+                self.shown_x[i] = self.x
+                self.shown_y[i] = self.y[i]
+
+            else:
+                self.shown_x[i] = []
+                self.shown_y[i] = []
+
+        for i in range(len(self.data_lines)):
+            #print(f"self.shown_x[{i}] = {self.shown_x[i]}, self.shown_y[{i}] = {self.shown_y[i]}")
+            self.data_lines[i].setData(self.shown_x[i], self.shown_y[i])
+
 
 
 if __name__ == '__main__':
